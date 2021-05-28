fa163fddfdd7a6bfe077aa230ec4594b539cd814
Allwyn Fernandes <allwynpfr@gmail.com>
2021-05-28 20:38:56 +0530

    play toggle and volume finally works

--- app.js
+++ app.js
@@ -36,85 +36,37 @@ const radio = new Howl({
 })
 
 
-// @type Number; Initializing counter; used by playStop function
-let counter = 0;
 
-
-
-
-
-// btnSndChatter.addEventListener("click", ()=> {
-//     counter++;
-//     if (counter%2==0) {
-//         console.log("Stop: "+counter);
-//         chatter.stop();
-//         btnSndChatter.classList.remove("playing")
-
-//     } else {
-//         console.log("Playing: "+counter);
-//         chatter.play();
-//         btnSndChatter.classList.add("playing")
-
-
-//     }
-// } );
-
-
-// Volume Control
-
-// let chatterVol = document.getElementById('vol-chatter');
-
-// chatterVol.addEventListener('input',function(){
-//     chatter.volume(this.value / 100);
-//     console.log(this.value/100);
-// });
-
-
-// Used with each sound to control playback on button click
-function playStop(counter, sound, button) {
-    if (counter%2==0) {
-        console.log("Stop: "+counter);
-        sound.stop();
-        button.classList.remove("playing")
-
-    } else {
-        console.log("Playing: "+counter);
+function togglePlayStop(sound, button, volbtn) {
+    if (!sound.playing()) {
         sound.play();
         button.classList.add("playing")
+    } else {
+        sound.stop();
+        button.classList.remove("playing")
+    };
+    let vol = document.getElementById(volbtn);
+    vol.addEventListener('input',function(){
+        sound.volume(this.value / 100);
+        console.log(this.value/100);
+    });
 
 
-    }
-
-}
-
-
-
-// Experimental! Not working as yet. Used to control volume on sounds.
-function controlVolume(volVal, sound) {
-    sound.volume(volVal / 100);
-    console.log(volVal/100);
-}
-
+};
 
 
 let btnSndChatter = document.getElementById("btn-snd-chatter");
-btnSndChatter.addEventListener("click", () => {counter++; playStop(counter, chatter, btnSndChatter)});
-let chatterVol = document.getElementById('vol-chatter');
+btnSndChatter.addEventListener("click", () => {togglePlayStop(chatter, btnSndChatter, 'vol-chatter')});
+
 
 
 let btnSndAmbiance = document.getElementById("btn-snd-ambiance");
-btnSndAmbiance.addEventListener("click", () => {counter++; playStop(counter, ambiance, btnSndAmbiance)});
+btnSndAmbiance.addEventListener("click", () => {togglePlayStop(ambiance, btnSndAmbiance, 'vol-ambiance')});
+
 
 
 let btnSndRadio = document.getElementById("btn-snd-radio");
-// In counter odd is play, even is pause hence the increment
-btnSndRadio.addEventListener("click", () => {counter++; playStop(counter, radio, btnSndRadio)});
-
-let radioVol = document.getElementById('vol-radio');
-radioVol.addEventListener('input',function(){
-    radio.volume(this.value / 100);
-    console.log(this.value/100);
-});
+btnSndRadio.addEventListener("click", () => {togglePlayStop(radio, btnSndRadio, 'vol-radio')});
 
 
 
---
+++ oldscript.js
@@ -0,0 +1,72 @@
+
+// @type Number; Initializing counter; used by playStop function
+
+
+
+
+
+// btnSndChatter.addEventListener("click", ()=> {
+//     counter++;
+//     if (counter%2==0) {
+//         console.log("Stop: "+counter);
+//         chatter.stop();
+//         btnSndChatter.classList.remove("playing")
+
+//     } else {
+//         console.log("Playing: "+counter);
+//         chatter.play();
+//         btnSndChatter.classList.add("playing")
+
+
+//     }
+// } );
+
+
+// Volume Control
+
+// let chatterVol = document.getElementById('vol-chatter');
+
+// chatterVol.addEventListener('input',function(){
+//     chatter.volume(this.value / 100);
+//     console.log(this.value/100);
+// });
+
+
+// Used with each sound to control playback on button click
+// let counter = 0;
+// function playStop(counter, sound, button) {
+//     if (counter%2==0) {
+//         console.log("Stop: "+counter);
+//         sound.stop();
+//         button.classList.remove("playing")
+
+//     } else {
+//         console.log("Playing: "+counter);
+//         sound.play();
+//         button.classList.add("playing")
+
+
+//     }
+
+// }
+
+
+
+
+
+// // Experimental! Not working as yet. Used to control volume on sounds.
+// function controlVolume(volVal, sound) {
+//     sound.volume(volVal / 100);
+//     console.log(volVal/100);
+// }
+
+
+
+// let chatterVol = document.getElementById('vol-chatter');
+
+
+// chatterVol.addEventListener("input", function() {
+//     volume = this.value
+//     console.log(volume/100);
+// });
+
